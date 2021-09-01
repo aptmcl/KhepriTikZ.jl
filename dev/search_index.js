b32360b502a4822a598017d7670e17d315d2e559
Documenter.jl <documenter@juliadocs.github.io>
2021-09-01 17:39:28 +0000

    build based on cc3650c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KhepriTikZ","category":"page"},{"location":"#KhepriTikZ","page":"Home","title":"KhepriTikZ","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KhepriTikZ]","category":"page"}]
+}
